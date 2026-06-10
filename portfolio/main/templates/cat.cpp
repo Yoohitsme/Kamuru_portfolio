67daabbfb725612b3a0f4a0bbb3df30d91a09604
Yoohitsme <vkamuru@gmail.com>
2026-06-10 12:08:11 +0300

    Added build.sh

---
+++ portfolio/main/templates/cat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Top vertical part
+    cout << "    X"    << endl;
+    cout << "    X"    << endl;
+    // Horizontal bar
+    cout << "X X X X X" << endl;
+    // Bottom vertical part
+    cout << "    X"    << endl;
+    cout << "    X"    << endl;
+    cout << "    X"    << endl;
+    cout << "    X"    << endl;
+
+    return 0;
+}
